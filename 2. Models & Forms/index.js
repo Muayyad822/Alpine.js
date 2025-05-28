16e43210ee0b98cc22567a7872033caf8be5555c
AbdulMuiz Jimoh <muayyad822@gmail.com>
2025-05-28 14:30:28 +0100

    Add form validation for signup fields

--- 2. Models & Forms/index.js
+++ 2. Models & Forms/index.js
@@ -1,14 +1,43 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('signupForm', () => ({
-        username:'',
+        username: '',
         password: '',
         passwordConfirm: '',
-        belt:'',
-        bio:'',
+        belt: '',
+        bio: '',
         newsletter: true,
         showPassword: false,
         showConfirmPassword: false,
-        submitForm(){
+        errors: {},
+
+        validateForm() {
+            this.errors = {}
+
+            if (this.username.length < 3) {
+                this.errors.username = "Username must be atleast 3 characters"
+            }
+
+            if (this.password.length < 6) {
+                this.errors.password = "Password must be atleast 6 characters"
+            }
+
+            if (this.password != this.passwordConfirm) {
+                this.errors.passwordConfirm = "Passwords do not match"
+            }
+
+            if (!this.belt) {
+                this.errors.belt = "Choose a belt color"
+            }
+
+            if (this.bio.length < 10) {
+                this.errors.bio = "Bio must be atleast 10 characters"
+            }
+
+        },
+
+        submitForm() {
+            this.validateForm()
+            console.log(this.errors);
             console.log(this.username, this.password, this.belt, this.bio, this.newsletter);
         }
     }))
